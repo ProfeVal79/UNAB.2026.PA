6f5a6b9cbe6237ddf39f2771c3dd9e477d92e586
Valeria Elizabeth Igarzabal <vigarzabal@abc.gob.ar>
2026-04-03 20:55:40 -0300

    agrego cuenta corriente

--- ejemplos/01.cuenta-bancaria/Cuenta_corriente.py
+++ ejemplos/01.cuenta-bancaria/Cuenta_corriente.py
@@ -8,7 +8,32 @@ class CuentaCorriente(CuentaBancaria):
     def extraer(self, monto):
         if monto <= (self.saldo + self.descubierto):
             self.saldo -= monto
-            self.movimientos.append(f"Extracción CC.: {self.moneda}{monto}")
+            self.movimientos.append(f"Extracción CC.: - {self.moneda}{monto}")
             return f"Extracción exitosa. Saldo actual: {self.moneda}{self.saldo}"
         else:
             return f"Rechazado. El monto supera tu saldo y tu descubierto de {self.moneda}{self.descubierto}"
+
+    def emitir_cheque(self, monto, beneficiario):
+        impuesto = monto * 0.06
+        total_a_descontar = monto + impuesto
+        if total_a_descontar <= (self.saldo + self.descubierto):
+            self.saldo -= total_a_descontar
+            self.movimientos.append(f"Cheque a {beneficiario} por - {self.moneda}{total_a_descontar}")
+            return f"se emitio cheque a {beneficiario}: {self.moneda}{monto} (Impuesto: {self.moneda}{impuesto})"
+        else:
+            return f"Cheque rechazado: saldo insuficiente para cubrir monto e impuestos"
+
+    def cobrar_mantenimiento(self, monto_mantenimiento):
+        self.saldo -= monto_mantenimiento
+        self.movimientos.append(f"Mantenimiento: - {self.moneda}{monto_mantenimiento}")
+        return f"Mantenimiento: - {self.moneda}{monto_mantenimiento}"
+
+    def depositar(self, monto):
+        self.saldo += monto
+        self.movimientos.append(f"Depósito: {self.moneda}{monto}")
+        return f"Deposito exitoso. Saldo actual: {self.moneda}{self.saldo}"
+
+    def recibir_transferencia(self, monto, emisor):
+        self.saldo += monto
+        self.movimientos.append(f"Transferencia de {emisor}: {self.moneda}{monto}")
+        return f"Recibiste {self.moneda}{monto} de {emisor}. Saldo actual: {self.saldo}"
--- ejemplos/01.cuenta-bancaria/test.py
+++ ejemplos/01.cuenta-bancaria/test.py
@@ -1,4 +1,6 @@
 from cuentaBancaria import CuentaBancaria
+print("MI CUENTA BANCARIA")
+print()
 #Creamos una cuenta y luego imprimimos los datos y el saldo
 cuenta = CuentaBancaria(500, "Valeria", "Igarzabal")
 print(cuenta.datos_titular())
@@ -14,8 +16,12 @@ print("extracción de $300")
 print(cuenta.extraer(300))
 print(cuenta.extraer(2000))
 print(f"Movimientos de la cuenta: ", cuenta.datos_movimientos())
+print("_"* 20)
 
 from cajaAhorro import CajaAhorro
+print()
+print(f"MI CAJA DE AHORRO")
+print()
 mi_ahorro = CajaAhorro(1000, "Valeria", "Igarzabal")
 print(mi_ahorro.datos_titular())
 print(mi_ahorro.pagar_con_beneficio(500, "almacen", "sabado"))
@@ -27,9 +33,19 @@ print(mi_ahorro.datos_saldo())
 print(mi_ahorro.datos_movimientos())
 print(mi_ahorro.aplicar_interes_mensual())
 print(mi_ahorro.datos_movimientos())
-
+print("_" * 20)
 #prueba de cuenta corriente
 from Cuenta_corriente import CuentaCorriente
+print()
+print(f" MI CUENTA CORRIENTE")
+print()
 mi_CC = CuentaCorriente(33000,"Valeria", "Igarzabal", 5000)
+print(mi_CC.datos_titular())
 print(mi_CC.extraer(10000))
+print(mi_CC.datos_movimientos())
+print(mi_CC.emitir_cheque(5000, "Pedro"))
+print(mi_CC.datos_movimientos())
+print(mi_CC.cobrar_mantenimiento(8000))
+print(mi_CC.depositar(80000))
+print(mi_CC.recibir_transferencia(90000, "Lucas"))
 print(mi_CC.datos_movimientos())
