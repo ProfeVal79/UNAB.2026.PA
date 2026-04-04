ce73164516017ecde587e26a66ae4213a3c5e7d9
Valeria Elizabeth Igarzabal <vigarzabal@abc.gob.ar>
2026-04-04 00:37:10 -0300

    resuelto clase animal y explicación (txt)

--- ejemplos/02.animales/animal.py
+++ ejemplos/02.animales/animal.py
@@ -1,28 +1,35 @@
 class Animal:
-
-    # Edad es un argumento opcional
+# Edad es un argumento opcional
     def __init__(self, nombre, edad = -1):
         self.nombre = nombre
         self.edad = edad
 
     def hablar(self):
-        print('Soy ' + self.nombre + ' y digo .....')
+        texto_hablar = "algo"
+        print(f'Soy {self.nombre}y digo {self.texto_hablar}')
+        
 
 class Perro(Animal):
     texto_hablar = 'Guau!'
 
     def hablar(self):
-        print('Soy ' + self.nombre + ' y digo ' + self.texto_hablar)   
+        print(f"Soy {self.nombre} y digo {self.texto_hablar}")   
+        
 
 class Gato(Animal):
+    texto_hablar = "Miau!"
     def hablar(self):
-        print('Soy ' + self.nombre + ' y digo Miau!')
+        print(f"soy {self.nombre} y digo {self.texto_hablar}")
 
 class Raton(Animal):
-    pass
+    texto_hablar = "¡Squeak!"
+
+    def hablar(self):
+          print(f"soy {self.nombre} y digo {self.texto_hablar}")
+
 
 class Gatito(Gato):
-    pass
+ pass
 
 for elem in Perro('Spike'), Gato('Tom'), Raton('Jerry', 3), Gatito("Mininito"): 
     elem.hablar()
